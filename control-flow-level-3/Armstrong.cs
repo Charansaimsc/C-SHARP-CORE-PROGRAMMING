66407cedbdeb263766b8af4f8e34bbad7fa7e36e
Charansaimsc <charanmsc7969@gmail.com>
2025-12-19 14:05:47 +0530

    [charan]Add.Solution Armstrong number

---
+++ control-flow-level-3/Armstrong.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace control_flow_Level_2
+{
+    public class ArmstrongNumber
+    {
+        public static void CheckArmstrong()
+        {
+            int number = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int originalNumber = number;
+
+            while (originalNumber != 0)
+            {
+                int remainder = originalNumber % 10;
+                sum += remainder * remainder * remainder;
+                originalNumber /= 10;
+            }
+
+            if (sum == number)
+            {
+                Console.WriteLine($"{number} is an Armstrong number.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} is not an Armstrong number.");
+            }
+        }
+    }
+}
