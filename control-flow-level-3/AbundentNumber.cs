0c0158d47208722cdd4379e5a61cb03afcd6e257
Charansaimsc <charanmsc7969@gmail.com>
2025-12-19 14:28:23 +0530

    [charan]Add.AbundentNumber problem

---
+++ control-flow-level-3/AbundentNumber.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class AbundantNumber
+{
+    public void Class1()
+    {
+        Console.WriteLine(" Enter a Number for Abundant Number : ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int sum = 0;
+
+        for (int i = 1; i < number; i++)
+        {
+            if (number % i == 0)
+            {
+                sum += i;
+            }
+        }
+
+        if (sum > number)
+        {
+            Console.WriteLine(" Number is a Abundant Number");
+        }
+        else
+        {
+            Console.WriteLine(" Number is not a Abundant Number");
+
+        }
+    }
+}
