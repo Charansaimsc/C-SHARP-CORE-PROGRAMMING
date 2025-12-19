fdbd2a8ea8697c19a8acabb03cbef9f9408b5e4f
Charansaimsc <charanmsc7969@gmail.com>
2025-12-19 14:22:36 +0530

    [charan]Add.Updated Main code Program.cs

--- control-flow-level-3/Program.cs
+++ control-flow-level-3/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace level_3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Armstrong arm = new Armstrong();
+            arm.Class1();
+
+            DigitCounter dc = new DigitCounter();
+            dc.Class1();
+
+            HarshadNumber hn = new HarshadNumber();
+            hn.Class1();
+
+            AbundantNumber an = new AbundantNumber();
+            an.Class1();
+
+            DayOfWeek1 dw = new DayOfWeek1();
+            dw.Class1();
+
+            Calculator cal = new Calculator();
+            cal.Class1();
+        }
+    }
+}
