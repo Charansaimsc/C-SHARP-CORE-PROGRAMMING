a3ddce602c8f2e58e8c46b4fbd31227ede29165e
Charansaimsc <charanmsc7969@gmail.com>
2025-12-19 15:05:52 +0530

    [charan] Add.Basic operation on DataTypes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Core_Programing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Simple console application for printing text and user input..
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+
+            // Data Types
+            int number = 10;
+            Console.WriteLine(number);
+            Console.WriteLine(int.MaxValue);
+            Console.WriteLine(int.MinValue);
+
+            long longNumber = 10L;
+            Console.WriteLine(longNumber);
+            Console.WriteLine(long.MaxValue);
+            Console.WriteLine(long.MinValue);
+
+
+            double doubleNumber = 15.00D;
+            Console.WriteLine(doubleNumber);
+            Console.WriteLine(double.MaxValue);
+            Console.WriteLine(double.MinValue);
+
+            decimal decimalNumber = 19999.99M;
+            Console.WriteLine(decimalNumber);
+            Console.WriteLine(decimal.MaxValue);
+            Console.WriteLine(decimal.MinValue);
+
+            byte b = 255;
+            short s = -32000;
+            Console.WriteLine(s);
+            Console.WriteLine(b);
+        }
+    }
+}
