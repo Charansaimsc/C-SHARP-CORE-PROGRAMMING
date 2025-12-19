04b021ca81bda2beee6ebd2174e4a5795ad33601
Charansaimsc <charanmsc7969@gmail.com>
2025-12-19 12:22:31 +0530

    [charan]Add.solution for Multiples problem

---
+++ control-flow-Level-2/muntiples.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace control_flow_Level_2
+{
+    public class Multiples
+    {
+        public static void Multiple()
+        {
+            int number = int.Parse(Console.ReadLine());
+            for (int i = number; i < 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
