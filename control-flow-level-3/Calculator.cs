820ad4151574d3d851b560eba897b5b0e31818f4
Charansaimsc <charanmsc7969@gmail.com>
2025-12-19 14:08:46 +0530

    [charan]Add.Solution Calulator problem

---
+++ control-flow-level-3/Calculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Calculator
+{
+    public void Class1()
+    {
+        Console.WriteLine("Enter Two numbers : ");
+        double first = Convert.ToDouble(Console.ReadLine());
+        double second = Convert.ToDouble(Console.ReadLine());
+        string op = Console.ReadLine();
+        switch (op)
+        {
+            case "+":
+                {
+                    Console.WriteLine("The addition is : " + (first + second));
+                    break;
+                }
+            case "-":
+                {
+                    Console.WriteLine("The Substracton is : " + (first - second));
+                    break;
+                }
+            case "*":
+                {
+                    Console.WriteLine("The Multiplication is : " + (first * second));
+                    break;
+                }
+            case "/":
+                {
+                    Console.WriteLine("The Division is : " + (first / second));
+                    break;
+                }
+        }
+
+    }
+}
