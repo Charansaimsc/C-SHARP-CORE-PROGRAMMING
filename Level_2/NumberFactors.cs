7283c6ccd33e4fbb1df5f66ecf229d6657d4b7f1
Charansaimsc <charanmsc7969@gmail.com>
2025-12-22 22:32:09 +0530

    [charan]Add.FactorsProblem with different Methods

---
+++ Level_2/NumberFactors.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+public class NumberFactors
+{
+	public static int[] FindFactors(int number)
+	{
+		int count = 0;
+		for(int i = 1; i <= number ; i++)
+		{
+			if (number % i == 0)
+			{
+				count++;
+			}
+
+		}
+		int[] factors = new int[count];
+        int index = 0;
+        for (int i = 1; i <= number; i++)
+		{
+		
+
+            if (number % i == 0)
+			{
+				factors[index] = i;
+				index++;
+			}
+        }
+				
+         return factors;
+	}
+		public static  int Sum(int[] factors) 
+		{
+		int sum = 0;
+		for(int i = 0; i < factors.Length; i++)
+		{
+			sum += factors[i];
+		}
+		return sum;
+
+	    }
+        public static  int Product(int[] factors)
+	    {
+		int product = 1;
+        for (int i = 0; i < factors.Length; i++)
+        {
+            product *= factors[i];
+        }
+        return product;
+
+        }
+	public static double PowerOfFactors(int[] factors)
+	{
+		double sum = 0;
+        for (int i = 0; i < factors.Length; i++)
+		{
+			sum += Math.Pow(factors[i],2);
+		}
+		return sum;
+    }
+
+	
+}
--- Level_2/Program.cs
+++ Level_2/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Level_2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+            int[] factors = NumberFactors.FindFactors(number);
+            for (int i = 0; i < factors.Length; i++)
+            {
+                Console.WriteLine(factors[i] + "  ");
+            }
+            Console.WriteLine(" \n"+NumberFactors.Sum(factors));
+            Console.WriteLine(NumberFactors.Product(factors));
+            Console.WriteLine(NumberFactors.PowerOfFactors(factors));
+        }
+    }
+
+}
