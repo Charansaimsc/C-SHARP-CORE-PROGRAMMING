483936fcb622b7b1b64558fb91d9d7fb9d497d22
Charansaimsc <charanmsc7969@gmail.com>
2025-12-22 16:52:59 +0530

    [charan]Add.Simple Interest Problem

---
+++ Level_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Level_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int principal = Convert.ToInt32(Console.ReadLine());
+            int rate = Convert.ToInt32(Console.ReadLine());
+            int time = Convert.ToInt32(Console.ReadLine());
+            SimpleInterest S = new SimpleInterest();
+            Console.WriteLine("the simple interest is : " + S.CalculateSI(principal,rate,time));
+        }
+    }
+}
---
+++ Level_1/SimpleInterest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class SimpleInterest
+{
+	public  int CalculateSI(int principal,int rate,int time)
+	{
+		int simpleInterst = (principal*rate*time)/ 100;
+		return simpleInterst;
+	}
+}
