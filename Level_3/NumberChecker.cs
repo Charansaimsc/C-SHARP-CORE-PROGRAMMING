ead5b54770be76840f9e960057da3b2f85d63768
Charansaimsc <charanmsc7969@gmail.com>
2025-12-23 14:53:19 +0530

    [charan]Add.NumberChecker Problem Solution

---
+++ Level_3/NumberChecker.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+public class NumberChecker
+{
+	public static int  NumberCheckerDigits(int number)
+	{
+		int count = 0;
+		while (number != 0)
+		{
+			number = number / 10;
+			count++;
+
+		}
+		return count;
+	}
+	public static int[] StoreDigits(int number)
+	{
+		int size = NumberCheckerDigits(number);
+		int[] digits = new int[size];
+		for (int i = 0; i < size; i++)
+		{
+			digits[i] = number % 10;
+			number = number / 10;
+
+
+		}
+		return digits;
+	}
+	public static bool DuckNumber(int[] digits)
+	{
+		for (int i = 0; i < digits.Length-1; i++)
+		{
+			if(digits[i] == 0){
+			return true;
+		}
+		}
+		return false;
+	}
+	public static void FindLargestSecondLargest(int[] digits)
+	{
+		int largest = int.MinValue;
+
+		int secLargest = int.MinValue;
+		for (int i = 0; i < digits.Length; i++)
+		{
+			if (digits[i] > largest)
+			{
+				secLargest = largest;
+				largest = digits[i];
+			}
+			else if (digits[i] > secLargest && largest != digits[i])
+			{
+				secLargest = digits[i];
+			}
+		}
+		Console.WriteLine(largest+"                                               "+secLargest);
+	}
+}
--- Level_3/Program.cs
+++ Level_3/Program.cs
@@ -5,21 +5,27 @@ namespace Level_3
     {
         static void Main(string[] args)
         {
-            int[] arr = new int[11];
-            Random rnd = new Random();
-            for(int i = 0; i < 11; i++)
-            {
-                arr[i] = rnd.Next(150,251);
-                Console.WriteLine(arr[i]);
-            }
+            //int[] arr = new int[11];
+            //Random rnd = new Random();
+            //for(int i = 0; i < 11; i++)
+            //{
+            //    arr[i] = rnd.Next(150,251);
+            //    Console.WriteLine(arr[i]);
+            //}
 
-            Console.WriteLine(ShortestTallestMean.Sum(arr));
-            Console.WriteLine(ShortestTallestMean.Mean(arr));
+            //Console.WriteLine(ShortestTallestMean.Sum(arr));
+            //Console.WriteLine(ShortestTallestMean.Mean(arr));
 
-            Console.WriteLine(ShortestTallestMean.Tallest(arr));
-            Console.WriteLine(ShortestTallestMean.Shortest(arr));
+            //Console.WriteLine(ShortestTallestMean.Tallest(arr));
+            //Console.WriteLine(ShortestTallestMean.Shortest(arr));
 
 
+            int number = 153;
+            Console.WriteLine(NumberChecker.NumberCheckerDigits(number));
+
+             int[] digits = NumberChecker.StoreDigits(number);
+            Console.WriteLine(NumberChecker.DuckNumber(digits) ? "Yes" : "No");
+            NumberChecker.FindLargestSecondLargest(digits);
         }
     }
 }
