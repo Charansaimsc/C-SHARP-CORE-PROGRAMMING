f6f89ce051abf987e7f7c064f382152298b83edf
Charansaimsc <charanmsc7969@gmail.com>
2025-12-23 10:23:28 +0530

    [Charan] Add.Sum of natural numbers problem

---
+++ Level_2/NaturalNumbersSum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class NaturalNumbersSum
+{
+	public  static int  WithoutUsingRecurrsion(int number )
+	{
+		int result = (number*number +number )/2;
+		return result;
+	}
+	public static  int UsingRecurrsion(int number)
+	{
+		int sum = 0;
+		if (number == 0)
+		{
+			return 0;
+		}
+		return number + UsingRecurrsion(number-1);
+
+	}
+
+}
--- Level_2/Program.cs
+++ Level_2/Program.cs
@@ -5,15 +5,28 @@ namespace Level_2
     {
         static void Main(string[] args)
         {
-            int number = Convert.ToInt32(Console.ReadLine());
-            int[] factors = NumberFactors.FindFactors(number);
-            for (int i = 0; i < factors.Length; i++)
-            {
-                Console.WriteLine(factors[i] + "  ");
-            }
-            Console.WriteLine(" \n"+NumberFactors.Sum(factors));
-            Console.WriteLine(NumberFactors.Product(factors));
-            Console.WriteLine(NumberFactors.PowerOfFactors(factors));
+            //int number = Convert.ToInt32(Console.ReadLine());
+            //int[] factors = NumberFactors.FindFactors(number);
+            //for (int i = 0; i < factors.Length; i++)
+            //{
+            //    Console.WriteLine(factors[i] + "  ");
+            //}
+            //Console.WriteLine(" \n"+NumberFactors.Sum(factors));
+            //Console.WriteLine(NumberFactors.Product(factors));
+            //Console.WriteLine(NumberFactors.PowerOfFactors(factors));
+
+
+            int number = int.Parse(Console.ReadLine());
+           Console.WriteLine( NaturalNumbersSum.UsingRecurrsion(number));
+
+            Console.WriteLine(NaturalNumbersSum.WithoutUsingRecurrsion(number));
+
+
+
+
+
+
+
         }
     }
 
