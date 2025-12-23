7ada349e2ba0eb5ac485e165c62a49c47a573991
Charansaimsc <charanmsc7969@gmail.com>
2025-12-23 14:16:46 +0530

    [charan]Add.Height Analyzer ProblemSolution

--- Level_3/Program.cs
+++ Level_3/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace Level_3
+{
+    public class Program 
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[11];
+            Random rnd = new Random();
+            for(int i = 0; i < 11; i++)
+            {
+                arr[i] = rnd.Next(150,251);
+                Console.WriteLine(arr[i]);
+            }
+
+            Console.WriteLine(ShortestTallestMean.Sum(arr));
+            Console.WriteLine(ShortestTallestMean.Mean(arr));
+
+            Console.WriteLine(ShortestTallestMean.Tallest(arr));
+            Console.WriteLine(ShortestTallestMean.Shortest(arr));
+
+
+        }
+    }
+}
---
+++ Level_3/ShortestTallestMeanHeight.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class ShortestTallestMean
+{
+	public static int Sum(int[] arr)
+	{
+		int sum = 0;
+		for (int i = 0; i < 11; i++) 
+		{
+			sum += arr[i];
+		}
+		return sum;
+	}
+	public static double Mean(int[] arr)
+	{
+		double sum = Sum(arr);
+		double mean = sum / arr.Length;
+		return mean;
+	}
+	public static int Shortest(int[] arr)
+	{
+		int shorter = arr[0];
+		for(int i = 0; i < 11; i++)
+		{
+			 shorter =Math.Min(shorter, arr[i]);
+		}
+		return shorter;
+	}
+	public static int Tallest(int[] arr)
+	{
+		int taller = 0 ;
+		for(int i = 0; i < 11; i++)
+		{
+			taller = Math.Max(taller, arr[i]);
+		}
+		return taller;
+	}
+
+}
