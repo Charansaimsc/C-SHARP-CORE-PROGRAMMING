3afcccdf1fad0afc0250fa6101f55f7c5602982d
Charansaimsc <charanmsc7969@gmail.com>
2025-12-23 15:29:25 +0530

    [charan]Add.NumberChecker2 solution

---
+++ Level_3/NumberChecker2.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+public class NumberChecker2
+{
+    public static int[] FindCountAndStore(int number)
+    {
+        int temp = number;
+        int count = 0;
+
+        while (temp != 0)
+        {
+            temp /= 10;
+            count++;
+        }
+
+        int[] digits = new int[count];
+        temp = number;
+
+        for (int i = 0; i < count; i++)
+        {
+            digits[i] = temp % 10;
+            temp /= 10;
+        }
+        return digits;
+    }
+
+    public static int SumOfDigits(int[] digits)
+    {
+        int sum = 0;
+        for (int i = 0; i < digits.Length; i++)
+        {
+            sum += digits[i];
+        }
+        return sum;
+    }
+
+    public static int SumOfSquares(int[] digits)
+    {
+        int sumSquares = 0;
+        for (int i = 0; i < digits.Length; i++)
+        {
+            sumSquares += digits[i] * digits[i];
+        }
+        return sumSquares;
+    }
+
+    public static bool HarshadNumber(int[] digits, int number)
+    {
+        int total = SumOfDigits(digits);
+        return number % total == 0;
+    }
+
+    // Correct frequency logic
+    public static int[,] FindFrequency(int[] digits)
+    {
+        int[,] freq = new int[10, 2];
+
+        for (int i = 0; i < 10; i++)
+        {
+            freq[i, 0] = i;
+            freq[i, 1] = 0;
+        }
+
+        for (int i = 0; i < digits.Length; i++)
+        {
+            freq[digits[i], 1]++;
+        }
+
+        return freq;
+    }
+}
--- Level_3/Program.cs
+++ Level_3/Program.cs
@@ -20,12 +20,27 @@ namespace Level_3
             //Console.WriteLine(ShortestTallestMean.Shortest(arr));
 
 
-            int number = 153;
-            Console.WriteLine(NumberChecker.NumberCheckerDigits(number));
+            //int number = 153;
+            //Console.WriteLine(NumberChecker.NumberCheckerDigits(number));
 
-             int[] digits = NumberChecker.StoreDigits(number);
-            Console.WriteLine(NumberChecker.DuckNumber(digits) ? "Yes" : "No");
-            NumberChecker.FindLargestSecondLargest(digits);
+            // int[] digits = NumberChecker.StoreDigits(number);
+            //Console.WriteLine(NumberChecker.DuckNumber(digits) ? "Yes" : "No");
+            //NumberChecker.FindLargestSecondLargest(digits);
+
+
+            int number = 95560;
+            int[] digits = NumberChecker2.FindCountAndStore(number);
+            Console.WriteLine(NumberChecker2.SumOfDigits(digits));
+            Console.WriteLine(NumberChecker2.SumOfSquares(digits));
+            Console.WriteLine(NumberChecker2.HarshadNumber(digits,number) ? "Yes" : " no");
+            int[,] freq = NumberChecker2.FindFrequency(digits);
+
+            Console.WriteLine("Digit Frequencies:");
+            for (int i = 0; i < 10; i++)
+            {
+                if (freq[i, 1] > 0)
+                    Console.WriteLine(freq[i, 0] + " → " + freq[i, 1]);
+            }
         }
     }
 }
